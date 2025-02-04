241d3540e1e7d978ea9ca3b6523c6e37ea9ab7e4
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-02-04 23:46:21 +0600

    Many to Many relationship have been implemented

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
 # Create your models here.
 class Tasks(models.Model):
     project = models.ForeignKey("Project", on_delete=models.CASCADE, default=1)
-    # title = models.CharField(max_length=250)
+    assigned_to = models.ManyToManyField(Employee)
+    title = models.CharField(max_length=250, default="")
     description = models.TextField()
     due_date = models.DateField()
     is_completed = models.BooleanField(default=False)
@@ -26,3 +31,6 @@ class TaskDetail(models.Model):
 class Project(models.Model):
     name = models.CharField(max_length=100)
     start_date = models.DateField()
+
+# task = onekgula employee ekta task
+# employee = onekgula task er jonno assign ache
