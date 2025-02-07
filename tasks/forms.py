764552c135e316f8851cbb3e60bb0e672e5dbcf2
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-02-07 19:16:37 +0600

    Dynamically populating relational fields using django forms and create task in db done

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,3 +5,9 @@ class TaskForm(forms.Form):
     title = forms.CharField(max_length=250, label="Task Title")
     description = forms.CharField( widget=forms.Textarea, label="Task Description")
     due_date = forms.DateField( widget=forms.SelectDateWidget(years=[2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025]))
+    assigned_to = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=[], label="Assigned to")
+
+    def __init__(self, *args, **kwargs):
+        employees = kwargs.pop("employees", [])
+        super().__init__(*args, **kwargs)
+        self.fields['assigned_to'].choices = [(emp.id, emp.name) for emp in employees]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from tasks.forms import TaskForm
+from tasks.models import Employee, Tasks
+from django.http import HttpResponse
 # Create your views here.
 
 def manager_dashboard(request):
@@ -16,6 +18,24 @@ def test(request):
     return render(request, "test.html", context)
 
 def create_task(request):
-    form = TaskForm()
+    employees = Employee.objects.all()
+    form = TaskForm(employees = employees) # For GET Request
+    if request.method == "POST":
+        form = TaskForm(request.POST, employees = employees)
+        if form.is_valid():
+            data = form.cleaned_data
+            title = data.get('title')
+            description = data.get('description')
+            due_date = data.get('due_date')
+            assigned_to = data.get('assigned_to')
+
+            task = Tasks.objects.create(title=title, description=description, due_date=due_date)
+            # Assign employees to task
+            for emp_id in assigned_to:
+                employee = Employee.objects.get(id=emp_id)
+                task.assigned_to.add(employee)
+            
+            return HttpResponse("Task added successfully")
+
     context = {"form": form}
-    return render(request,"task_form.html", context)
+    return render(request, "task_form.html", context)
