f5d6c20664f47b6f99746e41710b87b1064d8a4a
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-02-08 07:47:53 +0600

    Started using Django custom widget to style the form using tailwindCSS

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from tasks.models import Tasks
 
-
+# Regular Django Form
 class TaskForm(forms.Form):
     title = forms.CharField(max_length=250, label="Task Title")
     description = forms.CharField( widget=forms.Textarea, label="Task Description")
@@ -11,3 +12,28 @@ class TaskForm(forms.Form):
         employees = kwargs.pop("employees", [])
         super().__init__(*args, **kwargs)
         self.fields['assigned_to'].choices = [(emp.id, emp.name) for emp in employees]
+
+
+# Django Model Form
+class TaskModelForm(forms.ModelForm):
+    class Meta:
+        model = Tasks
+        fields = ['title', 'description', 'due_date', 'assigned_to']
+        # exclude = ['project', 'is_completed', 'created_at', 'updated_at']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'border-2 border-gray-500 p-2 mb-2 w-full rounded-lg shadow-sm focus:border-rose-400',
+                'placeholder': 'Enter task title'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'border-2 border-gray-500 p-2 mb-2 w-full rounded-lg shadow-sm focus:border-rose-400',
+                'placeholder': 'Describe the task'
+            }),
+            'due_date': forms.SelectDateWidget(attrs={
+                'class': 'border-2 border-gray-500 p-2 mb-2 rounded-lg shadow-sm focus:border-rose-400',
+                'placeholder': 'Describe the task'
+            }),
+            'assigned_to': forms.CheckboxSelectMultiple(attrs={
+                'class': 'mb-2'
+            })
+        }
