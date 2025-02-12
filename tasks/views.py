da3aaa84c1b6d20f760c3be7d5520c9d9fa45a27
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-02-12 21:28:31 +0600

    prefetch_related used to query through many to many field

--- tasks/views.py
+++ tasks/views.py
@@ -87,5 +87,7 @@ def view_task(request):
     # tasks = Tasks.objects.select_related('project').all()
     
     """ prefetch_related (reverce foreignkey, many to many) """
-    tasks = Project.objects.prefetch_related("tasks").all()
+    # tasks = Project.objects.prefetch_related("tasks").all()
+    # tasks = Tasks.objects.prefetch_related("assigned_to").all()
+    tasks = Employee.objects.prefetch_related("tasks").all()
     return render(request, "show_task.html", {"tasks": tasks})
