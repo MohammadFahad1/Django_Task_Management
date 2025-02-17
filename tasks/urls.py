2d7499ae0bf5b64e362f3bd19dfeca3c48cc3c99
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-02-17 07:20:47 +0600

    Delete Query done successfully

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from tasks.views import create_task, manager_dashboard, user_dashboard, test, view_task, update_task
+from tasks.views import create_task, manager_dashboard, user_dashboard, test, view_task, update_task, delete_task
 
 urlpatterns = [
     path('manager-dashboard/', manager_dashboard, name='manager-dashboard'),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('test', test),
     path('create-task', create_task, name='create-task'),
     path('view-task', view_task),
-    path('update-task/<int:id>/', update_task, name='update-task')
+    path('update-task/<int:id>/', update_task, name='update-task'),
+    path('delete-task/<int:id>/', delete_task, name='delete-task')
 ]
