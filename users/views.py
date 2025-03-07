2627455e9fd5bb8a3d4f01fc49ebf378b052da42
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-03-08 00:23:23 +0600

    Registration process initial setup using UserCreationForm

--- users/views.py
+++ users/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def sign_up(request):
-    return render(request, 'registration/register.html')
+    if request.method == 'GET':
+        form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    return render(request, 'registration/register.html', {"form": form})
