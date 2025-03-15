79c73df113315c7e579ce642ad19267dee3b4520
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-03-15 19:16:50 +0600

    Secret Details are moved into .env file

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,6 @@
 from asyncio import Task
 from django.db import models
-from django.db.models.signals import post_save, pre_save, m2m_changed
+from django.db.models.signals import post_save, pre_save, m2m_changed, post_delete
 from django.dispatch import receiver
 from django.core.mail import send_mail
 
@@ -46,7 +46,8 @@ class TaskDetail(models.Model):
         (MEDIUM, "Medium"),
         (LOW, "Low")
     )
-    task = models.OneToOneField(Tasks, on_delete=models.CASCADE, related_name="task_detail")
+    # task = models.OneToOneField(Tasks, on_delete=models.CASCADE, related_name="task_detail")
+    task = models.OneToOneField(Tasks, on_delete=models.DO_NOTHING, related_name="task_detail")
     # assigned_to = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
     notes = models.TextField(blank=True, null=True)
@@ -96,3 +97,10 @@ def notify_employees_on_task_creation(sender, instance, action, **kwargs):
             assigned_emails,
             fail_silently=False,
         )
+
+@receiver(post_delete, sender=Tasks)
+def delete_associate_details(sender, instance, **kwargs):
+    if instance.task_detail:
+        instance.task_detail.delete()
+
+        print("Deleted successfully")
