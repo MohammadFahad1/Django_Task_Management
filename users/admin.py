ceb0af3ee6ebdeb605a8af12b8d304edc4e6384a
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-09-13 04:09:59 +0600

    Stucked in error while saving userprofile info while registration

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from users.models import UserProfile
 
 # Register your models here.
+admin.site.site_header = "Task Management Admin"
+admin.site.site_title = "Task Management Admin Portal"
+admin.site.index_title = "Welcome to Task Management Admin Portal"
+admin.site.register(UserProfile)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+import re
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile',primary_key=True),
+    profile_images = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
