23bdcc407c698441dfea27c4ad8d387379cb6f4f
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-09-15 23:14:11 +0600

    User profile error solved

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile',primary_key=True),
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile',primary_key=True)
     profile_images = models.ImageField(upload_to='profile_images/', blank=True, null=True)
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=100, blank=True)
