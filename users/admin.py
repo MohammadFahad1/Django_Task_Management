a17ebe78e348428f497b7cf3aa4e524194729e4f
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-09-27 23:52:56 +0600

    The database re-created to solve the errors

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-# from users.models import UserProfile
+from users.models import CustomUser
 
 # Register your models here.
 admin.site.site_header = "Task Management Admin"
 admin.site.site_title = "Task Management Admin Portal"
 admin.site.index_title = "Welcome to Task Management Admin Portal"
-# admin.site.register(UserProfile)
+admin.site.register(CustomUser)
