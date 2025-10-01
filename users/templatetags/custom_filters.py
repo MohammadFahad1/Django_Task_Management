cd2438aeecdf504593d42aabbfd84310b669fdec
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-10-01 18:36:06 +0500

    Custom templatetag issue fixed

--- users/templatetags/custom_filters.py
+++ users/templatetags/custom_filters.py
@@ -1,5 +1,5 @@
 from django import template
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.utils import timezone
 
 register = template.Library()
@@ -7,14 +7,18 @@ register = template.Library()
 @register.filter
 def humanized_date(value):
     if value:
-        today = datetime.now().date()
-        value = timezone.localtime(value)
-        if value.date() == today:
+        today = datetime.now(timezone.get_current_timezone()).date()
+        value_local = timezone.localtime(value)
+        value_date = value_local.date()
+
+        yesterday = today - timedelta(days=1)
+
+        if value_date == today:
             return f"Today at {value.strftime('%I:%M %p')}"
-        elif value.date() == today.replace(day=today.day  - 1):
-            return f"Yesterday at {value.strftime('%I:%M %p')}"
-        elif value.year == today.year:
-            return value.strftime('%B %d at %I:%M %p')
+        elif value_date == yesterday:
+            return f"Yesterday at {value_date.strftime('%I:%M %p')}"
+        elif value_date.year == today.year:
+            return value_date.strftime('%B %d at %I:%M %p')
         else:
-            return value.strftime('%B %d, %Y at %I:%M %p')
+            return value_date.strftime('%B %d, %Y at %I:%M %p')
     return ''
